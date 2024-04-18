46a7352757376e0a8ba8c4b6ab8b5e271f12dde0
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-04-18 23:09:08 +0300

    Update install_nginx.sh

--- 0x12-web_stack_debugging_2/install_nginx.sh
+++ 0x12-web_stack_debugging_2/install_nginx.sh
@@ -1,17 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Configures a new ubuntu machine by installing
+# Nginx where it should be listening on port 80
+# Serve a page that would return a Hello World string
+#
 
-# Update package list
-sudo apt update
+echo -e "Updating and installing Nginx.\n"
+sudo apt-get update -y -qq && \
+	 sudo apt-get install nginx -y
 
-# Install nginx
-sudo apt install -y nginx
+echo -e "\nSetting up some minor stuff.\n"
 
-# Start nginx service
-sudo systemctl start nginx
+# starting nginx service
+sudo service nginx start
 
-# Enable nginx to start on boot
-sudo systemctl enable nginx
+# allowing nginx on firewall
+sudo ufw allow 'Nginx HTTP'
 
-# Check nginx status
-echo "Nginx installation complete. Checking nginx status..."
-sudo systemctl status nginx
+# Give the user ownership to website files for easy editing
+sudo chown -R "$USER":"$USER" /var/www/html
+sudo chmod -R 755 /var/www
+
+# Backup default index
+cp /var/www/html/index.nginx-debian.html /var/www/html/index.nginx-debian.html.bckp
+
+# Creating new index
+echo -e "Hello World!" | dd status=none of=/var/www/html/index.nginx-debian.html
+
+# Restarting nginx
+sudo service nginx restart
+
+echo -e "\nCompleted. ✅\n"
